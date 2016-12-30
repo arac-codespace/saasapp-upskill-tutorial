62414e2c405ab006a9590af5313df05894858824
Alex.Aguirre <alex.y2kare@gmail.com>
2016-12-30 22:34:35 +0000

    Added fiel field for image upload to profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,5 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  class Profile < ActiveRecord::Base
-  belongs_to :user
-  has_attached_file :avatar,
-                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\z/
-  end
-    
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
